d5b9320efb2e3175d25d586722db983c68cb98d7
Isak Fyksen <isak@obsidian.no>
2020-12-28 01:34:15 +0100

    Add Lexer

--- premake5.lua
+++ premake5.lua
@@ -3,22 +3,21 @@ workspace "Naja"
                    , "Release"
                    }
 
-project "Naja"
-    location "src"
-    kind "ConsoleApp"
-    language "C"
-    links "flex_bison"
+project "Lexer"
+    location   "src/Lexer"
+    kind       "SharedLib"
+    language   "C++"
+    cppdialect "C++14"
+    toolset    "clang"
     
     targetdir "bin/%{prj.name}"
     objdir    "bin-int/%{prj.name}"
 
-    includedirs { "%{prj.location}" }
+    includedirs { "src" }
 
-    files { "%{prj.location}/**.c"
-          , "%{prj.location}/**.h"
+    files { "%{prj.location}/**.cpp"
+          , "%{prj.location}/**.hpp"
           }
-
-    buildoptions { "-lfl" }
 
     filter "configurations:Debug"
         defines { "DEBUG" }
@@ -27,14 +26,3 @@ project "Naja"
     filter "configurations:Release"
         defines { "NDEBUG" }
         optimize "On"
-
-
-project "flex_bison"
-    location "src/flex_bison"
-    kind "Utility"
-
-    prebuildcommands
-    { "bison -d -o %{prj.location}/parser.c %{prj.location}/parser.y"
-    , "flex -o %{prj.location}/scanner.c %{prj.location}/scanner.l"
-    --header-file=%{prj.location}/scanner.h
-    }
