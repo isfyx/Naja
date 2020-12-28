f71a3835c9b798fcdc1c7b7edd4c9e2af029ed71
Isak Fyksen <isak@obsidian.no>
2020-12-28 17:12:08 +0100

    Update main

--- src/Naja/Naja.cpp
+++ src/Naja/Naja.cpp
@@ -2,17 +2,21 @@
 #include <cstring>
 #include <iostream>
 #include <fstream>
+#include <memory>
 
 #include "Lexer/Lexer.hpp"
+#include "Lexer/Token.hpp"
+
+using namespace Naja;
 
 int main(int argc, char** argv)
 {
-    Naja::Lexer* lexer = nullptr;
+    std::unique_ptr<Lexer> lexer;
     
     if (argc > 1) {
         std::ifstream in(argv[1]);
         if (in.is_open())
-            lexer = new Naja::Lexer(in);
+            lexer = std::move(std::make_unique<Lexer>(in));
         else {
             std::cerr << "Failed to open '"
                       << argv[1]
@@ -21,11 +25,10 @@ int main(int argc, char** argv)
                       << std::endl;
             exit(errno);
         }
-    } else {
-        lexer = new Naja::Lexer(std::cin);
-    }
+    } else
+        lexer = std::move(std::make_unique<Lexer>(std::cin));
 
-    while (lexer->next());
-    
-    delete lexer;
+    std::unique_ptr<Token> currentToken;
+    while ((currentToken = std::move(lexer->next())) != nullptr)
+        currentToken->print();
 }
